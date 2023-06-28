9504e49334dd467288222d4250637353d0b304f6
Mariya Kor <nir21@ya.ru>
2023-06-29 02:56:49 +0300

    Added Task 02, 04, 06, 08

---
+++ Task 08/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+Console.WriteLine("Введено некорректное число, необходимо целое положительное.");  
+}
+else if (number == 1)
+{
+Console.WriteLine("Введено число 1, нет чётных чисел.");   
+}
+else
+{
+int count = 2;
+while (count < number)
+{
+   Console.Write($"{count} "); 
+   count+=2;                    
+} 
+}
