c231b1628db4b24cfbdb918f4acab5f172bb36d0
Mariya Kor <nir21@ya.ru>
2023-06-29 01:07:06 +0300

    Added Task00, Task01

--- Task 00/Program.cs
+++ Task 00/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает его и выдаёт его квадрат.
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// - 2 147 483 648 до 2 147 483 647
+// 2 вариант преобразования: int number1 = int.Parse(Console.ReadLine());
+int square = number * number;
+Console.Write($"Квадрат числа {number} = {square}");
---
+++ Task 01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и проверячет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = number2 * number2;
+if (number1 == square)
+{
+Console.WriteLine("Первое число является квадратом второго");    
+}
+else
+{
+Console.WriteLine("Первое число не является квадратом второго");     
+}
