53c63dfec1d43ceab478f522bcdebed3a8be459a
Mariya Kor <nir21@ya.ru>
2023-06-30 21:02:35 +0300

    Added Tasks 09-16

---
+++ Task 11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+
+int number = new Random().Next(100, 1000); 
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine($"Результат: {result}");
